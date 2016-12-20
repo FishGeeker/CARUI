291a6ecd4f1cdadc0c731595579849a1cafc034b
Hui <florianzhang@126.com>
2016-12-20 16:37:39 +0100

    clean up

---
+++ Assets/Scripts/OnCallNumber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OnCallNumber : MonoBehaviour {
+	public Text onCallNumber;
+
+	void start()
+	{
+		onCallNumber.text="jkljlk";
+	}
+}
